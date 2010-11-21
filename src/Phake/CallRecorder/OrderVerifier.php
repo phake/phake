2c358b00d0e64c3cdf26cc34bdd1c625e5676721
Mike <mjlivelyjr@gmail.com>
2010-11-20 16:37:17 -0800

    Fixing an E_STRICT error

--- src/Phake/CallRecorder/OrderVerifier.php
+++ src/Phake/CallRecorder/OrderVerifier.php
@@ -52,7 +52,8 @@ class Phake_CallRecorder_OrderVerifier
 	 */
 	public function verifyCallsInOrder(array $calls)
 	{
-		$call2 = array_shift(array_shift($calls));
+		$call1 = array_shift($calls);
+		$call2 = array_shift($call1);
 
 		while (count($calls))
 		{
