6d579545d22e19b1c72c0bfdecb7ef88b8019adf
Mike Lively <mjlivelyjr@gmail.com>
2012-01-01 09:25:20 -0800

    Adding a unit test for #36

---
+++ tests/Phake/Matchers/IMethodMatcher.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mikel
+ * Date: 1/1/12
+ * Time: 9:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class IMethodMatcher
+{
+
+}
