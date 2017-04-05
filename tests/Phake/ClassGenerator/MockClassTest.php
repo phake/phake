3c05b2e14e14f0fa5a62ae618fbc5ba3d7025781
Martijn Smit <smit@mollie.com>
2017-04-05 11:39:09 +0200

    Added test and exception for mocking final classes

--- tests/Phake/ClassGenerator/MockClassTest.php
+++ tests/Phake/ClassGenerator/MockClassTest.php
@@ -535,6 +535,18 @@ class Phake_ClassGenerator_MockClassTest extends PHPUnit_Framework_TestCase
         $this->addToAssertionCount(1);
     }
 
+    public function testMockFinalClassThrowsException()
+    {
+        $expectedException = new InvalidArgumentException("Final classes cannot be mocked.");
+
+        try {
+            $mock = Phake::mock('PhakeTest_FinalClass');
+            $this->fail("The mocked final method did not throw an exception");
+        } catch(InvalidArgumentException $actualException) {
+            $this->assertEquals($actualException, $expectedException);
+        }
+    }
+
     /**
      * Tests that the mocked object's __toString() method returns a string by default.
      */
@@ -787,7 +799,6 @@ class Phake_ClassGenerator_MockClassTest extends PHPUnit_Framework_TestCase
             $this->assertSame($expectedException, $actualException, "The same exception was not thrown");
         }
     }
-
 
     public function testVoidStubCanCallParent()
     {
