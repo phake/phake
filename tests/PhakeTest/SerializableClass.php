acc735d97428003300b471b45ae7bb0780f82e0c
Pierrick Charron <pierrick@webstart.fr>
2021-11-20 11:01:39 -0500

    Serializable interface is deprecated in PHP8.1

--- tests/PhakeTest/SerializableClass.php
+++ tests/PhakeTest/SerializableClass.php
@@ -1,16 +1,32 @@
 <?php
 
+if (version_compare(PHP_VERSION, '8.1.0', '>=')) {
 
-class PhakeTest_SerializableClass implements \Serializable
-{
-    public function serialize()
+    class PhakeTest_SerializableClass implements \Serializable
     {
+        public function serialize()
+        {
 
+        }
+
+        public function unserialize($serialized)
+        {
+
+        }
     }
 
-    public function unserialize($serialized)
+} else {
+
+    class PhakeTest_SerializableClass
     {
+        public function __serialize()
+        {
 
+        }
+
+        public function __unserialize($serialized)
+        {
+
+        }
     }
-
-} 
+}
