9e09fdea2e2fef249badb3709b27afe393879390
Tim Düsterhus <timwolla@googlemail.com>
2026-02-09 23:44:06 +0100

    Fix `SmartDefaultAnswerTest::testCallableReturn()` (#333)
    Closures cannot be meaningfully compared for equality. Test for `instanceof
    Closure` instead.
    
    see sebastianbergmann/comparator#129

--- tests/Phake/Stubber/Answers/SmartDefaultAnswerTest.php
+++ tests/Phake/Stubber/Answers/SmartDefaultAnswerTest.php
@@ -93,8 +93,7 @@ class SmartDefaultAnswerTest extends TestCase
         $context = new \PhakeTest_ScalarTypes();
         $cb = $this->answer->getAnswerCallback($context, 'callableReturn');
 
-        $this->assertEquals(function () {
-        }, $cb());
+        $this->assertInstanceOf(\Closure::class, $cb());
     }
 
     public function testObjectReturn()
